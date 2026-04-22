ee77962c5e1bf784a52d168f787074b83a2536ae
Alex Tully <alex.w.tully@student.uts.edu.au>
2026-04-22 17:57:31 +1000

    t

--- ios_ass3/ContentView.swift
+++ ios_ass3/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! hellsaojdffzvxcEFGgrfdhszvas -ppppppp")
+            Text("Hello, world! hellsaojdffzvxcEFGgrfdhszvas -ppppppp-ppppppppp")
         }
         .padding()
     }
