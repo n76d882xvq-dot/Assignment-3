744e717ca3f2622c8662dd7360fbfbcc65b17998
Ryan <ryan.a.nolan@student.uts.edu.au>
2026-04-22 17:50:31 +1000

    Initial Commit

---
+++ ios_ass3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_ass3
+//
+//  Created by Ryan Nolan on 22/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! hellsaojdffzvxczvas")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
